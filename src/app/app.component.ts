6e8f51b8329c9cb0d214604f21485211186fa7d0
Viktor Bagány <baganyviktor@gmail.com>
2021-02-13 13:43:38 +0100

    Add dependencies, start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'status-monitor';
+  cards: any = [
+    { title: 'CPU Usage' },
+    { title: 'Memory Usage' },
+    { title: 'Requests' },
+    { title: 'Visitors' },
+    { title: 'Card name' },
+    { title: 'Card name' },
+    { title: 'Card name' },
+    { title: 'Card name' },
+  ];
 }
