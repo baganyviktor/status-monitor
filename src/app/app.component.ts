c7a99ef9dfd13e9de04b21daf6b5a5fccf05c800
Viktor Bagány <baganyviktor@gmail.com>
2021-02-20 09:56:46 +0100

    graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+export class ArchitectureInfo {
+  year: number;
+  smp: number;
+  mmp: number;
+  cnstl: number;
+  cluster: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,14 +15,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'status-monitor';
-  cards: any = [
-    { title: 'CPU Usage' },
-    { title: 'Memory Usage' },
-    { title: 'Requests' },
-    { title: 'Visitors' },
-    { title: 'Card name' },
-    { title: 'Card name' },
-    { title: 'Card name' },
-    { title: 'Card name' },
+  compactView: boolean = true;
+
+  architecturesInfo: ArchitectureInfo[] = [
+    {
+      year: 1999,
+      smp: 169,
+      mmp: 256,
+      cnstl: 66,
+      cluster: 7,
+    },
+    {
+      year: 2001,
+      smp: 57,
+      mmp: 257,
+      cnstl: 143,
+      cluster: 43,
+    },
+    {
+      year: 2003,
+      smp: 0,
+      mmp: 163,
+      cnstl: 127,
+      cluster: 210,
+    },
+    {
+      year: 2005,
+      smp: 0,
+      mmp: 103,
+      cnstl: 36,
+      cluster: 361,
+    },
+    {
+      year: 2007,
+      smp: 0,
+      mmp: 91,
+      cnstl: 3,
+      cluster: 406,
+    },
   ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DxChartModule, DxSelectBoxModule } from 'devextreme-angular';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    DxChartModule,
+    DxSelectBoxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
